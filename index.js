81e22b4d04f258efbfe8440661a8494c6db707be
Soumitra Mishra <soumitra.mishra@newtonschool.co>
2022-01-31 20:47:51 +0530

    adding null check if clever tap events is called before initialise

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-var ClevertapReact = {
+const ClevertapReact = {
 
   initialize: function (accountId) {
     window.clevertap = { event: [], profile: [], account: [], onUserLogin: [], notifications: [] };
     window.clevertap.account.push({ 'id': accountId });
     (function () {
-      var wzrk = document.createElement('script');
+      const wzrk = document.createElement('script');
       wzrk.type = 'text/javascript';
       wzrk.async = true;
-      wzrk.src = ('https:' == document.location.protocol ? 'https://d2r1yp2w7bby2u.cloudfront.net' : 'http://static.clevertap.com') + '/js/a.js';
-      var s = document.getElementsByTagName('script')[0];
+      wzrk.src = ('https:' === document.location.protocol ? 'https://d2r1yp2w7bby2u.cloudfront.net' : 'http://static.clevertap.com') + '/js/a.js';
+      const s = document.getElementsByTagName('script')[0];
       s.parentNode.insertBefore(wzrk, s);
     })();
   },
 
   event: function (name, payload) {
+    if(!window.clevertap) return;
     if (payload) {
       window.clevertap.event.push(name, payload);
     } else {
@@ -23,10 +24,12 @@ var ClevertapReact = {
   },
 
   profile: function (payload) {
+    if(!window.clevertap) return;
     window.clevertap.profile.push(payload);
   },
 
   logout: function () {
+    if(!window.clevertap) return;
     window.clevertap.logout();
   }
 };
